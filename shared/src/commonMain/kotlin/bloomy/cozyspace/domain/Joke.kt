0ce6a5ac77788560e401637da890bd56004e4c76
Neogduh <mael.roustit@epita.fr>
2026-06-21 22:45:38 +0200

    chore: remove old joke code

--- shared/src/commonMain/kotlin/bloomy/cozyspace/domain/Joke.kt
+++
@@ -1,6 +0,0 @@
-package bloomy.cozyspace.domain
-
-data class Joke(
-    val text: String,
-    val answer: String,
-)
--- shared/src/commonMain/kotlin/bloomy/cozyspace/network/ApiService.kt
+++ shared/src/commonMain/kotlin/bloomy/cozyspace/network/ApiService.kt
@@ -1,14 +1,12 @@
 package bloomy.cozyspace.network
 
 import bloomy.cozyspace.config.Environment
-import bloomy.cozyspace.data.JokeDto
 import bloomy.cozyspace.data.LoginRequestDto
 import bloomy.cozyspace.data.RegisterRequestDto
 import bloomy.cozyspace.data.RegisterDto
 import bloomy.cozyspace.data.LoginDto
 import bloomy.cozyspace.interfaces.ApiResult
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.header
@@ -20,21 +18,14 @@ import io.ktor.http.contentType
 class ApiService(
     private val client: HttpClient
 ) {
-    suspend fun getRandomJoke(): JokeDto {
-        return getJoke("/random")
-    }
-
-    suspend fun getRandomJokeByType(type: String): JokeDto {
-        return getJoke("/type/$type/random")
-    }
-
-    private suspend fun getJoke(path: String): JokeDto {
-        return client
-            .get("${Environment.API_URL}$path") {
-                header(HttpHeaders.Authorization, "Bearer ${Environment.API_TOKEN}")
-            }
-            .body()
-    }
+//    Headers Example
+//    private suspend fun get(path: String): Dto {
+//        return client
+//            .get("${Environment.API_URL}$path") {
+//                header(HttpHeaders.Authorization, "Bearer ${Environment.API_TOKEN}")
+//            }
+//            .body()
+//    }
 
     suspend fun signup(
         request: RegisterRequestDto
